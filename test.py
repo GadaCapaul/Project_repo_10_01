4acd10dbd9689f271d118699a9e977daab642683
Gada Capaul <gadacapaul@eduroamstud-10-255-206-119.unisg.ch>
2025-04-14 17:23:00 +0200

    einige Zeilen hinzugefügt

--- test.py
+++ test.py
@@ -2,8 +2,10 @@ import streamlit as st
 st.write("Neue Webseite")
 st.write("Zweite Zeile")
 st.write("dritte Zeile")
-st.write("4 Zeile")
-st.write("4 Zeile")
+st.write("vierte Zeile")
+st.write("fünfte Zeile")
+print("hello")
+
 
 
 
